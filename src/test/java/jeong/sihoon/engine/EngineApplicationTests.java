fb153f8766fb6cee0511717bcc30c409264acaad
korea-sihoon-jeong <korea.sihoon.jeong@gmail.com>
2020-10-23 01:21:18 +0900

    스프링 부트 프로젝트 시작

---
+++ src/test/java/jeong/sihoon/engine/EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package jeong.sihoon.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
